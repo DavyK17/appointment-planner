c63f1c9a36292dd2fb5068ba50ee562eaf913912
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-08-02 23:37:16 +0300

    Step 4

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,30 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={({target: { value }}) => setName(value)}
+        required
+        placeholder="Name"
+      />
+      <input
+        type="tel"
+        value={phone}
+        onChange={({target: { value }}) => setPhone(value)}
+        pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+        required
+        placeholder="Phone number"
+      />
+      <input
+        type="email"
+        value={email}
+        onChange={({target: { value }}) => setEmail(value)}
+        required
+        placeholder="Email address"
+      />
+      <input type="submit" />
+    </form>
   );
 };
