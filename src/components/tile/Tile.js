50813aca0feb3b180ef9dfbaa8795d8a7cb9dacc
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-08-03 00:09:55 +0300

    Step 6

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ data }) => {
+  const values = Object.values(data);
+
   return (
     <div className="tile-container">
-      
+      {values.map((v, i) => <p key={i} className={i === 0 ? "tile-title" : "tile"}>{v}</p>)}
     </div>
   );
 };
